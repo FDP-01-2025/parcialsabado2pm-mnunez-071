848ebdf124d91a57f138a90d580f96c04fcdc4b2
mnunez-071 <00070822@uca.edu.sv>
2025-06-07 14:16:09 -0600

    00070822

---
+++ ejer1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,i;
+    cout << "ingrese un numero: ";
+    cin >> n;
+    i=0;
+    while (i<= n){
+        if (i%2==0)cout << i << " ";
+    
+    i++;
+    }
+    return 0;
+}
